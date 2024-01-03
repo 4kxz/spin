826a295e25936450a79878f0f5708d76b2bc56fa
Carlos Garcia-Marquez <carlos@lkxz.net>
2024-01-03 22:08:43 +0100

    Better controls

--- Ball.gd
+++ Ball.gd
@@ -1,19 +1,35 @@
 extends RigidBody2D
 
-@export var particles: GPUParticles2D
-@export var dust_velocity := 400.0
+@export var dust_velocity := 1000.0
+@export var particles_per_second := 24
+@export var dust_particles: GPUParticles2D
+@export var blur_start := 15
+@export var blur_intensity := 0.05
+@export var wheel_sprite: Sprite2D
+
+var next_particle := 0.0
+var velocity := 0.0
+
+
+func _physics_process(delta: float) -> void:
+	next_particle += delta
+	var blur_amount := maxf(0.0, absf(angular_velocity) - blur_start) * blur_intensity
+	wheel_sprite.material.set_shader_parameter("amount", blur_amount)
 
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	var length = linear_velocity.length()
-	if length < dust_velocity:
+	var prev_velocity = velocity
+	velocity = linear_velocity.length()
+	if prev_velocity < dust_velocity:
 		return
-	for i in state.get_contact_count():
-		if randf() < length / dust_velocity * 0.15:
-			particles.emit_particle(
-				Transform2D(0, state.get_contact_collider_position(i)),
-				-linear_velocity / 5.0,
-				Color.WHITE,
-				Color.WHITE,
-				GPUParticles2D.EmitFlags.EMIT_FLAG_POSITION | GPUParticles2D.EMIT_FLAG_VELOCITY,
-			)
+	if next_particle < (1.0 / particles_per_second):
+		return
+	if state.get_contact_count():
+		next_particle = 0.0
+		dust_particles.emit_particle(
+			Transform2D(0, state.get_contact_collider_position(0)),
+			-linear_velocity / 5.0,
+			Color.WHITE,
+			Color.WHITE,
+			GPUParticles2D.EmitFlags.EMIT_FLAG_POSITION | GPUParticles2D.EMIT_FLAG_VELOCITY,
+		)
--- Controller.gd
+++ Controller.gd
@@ -3,34 +3,35 @@ extends Node2D
 const GEAR_RATIOS = [2.0, 1.0]
 const GEAR_COLORS = [Color.BLUE, Color.RED]
 
-@onready var line: Line2D = $Line2D
 @export var body: RigidBody2D
 @export var force_multiplier := 2500
 @export var jump_multiplier := 500
 @export var camera: PhantomCamera2D
+@export var camera_target_sensitivity := 2.0
+@export var control_point: ControlPoint
 
+var gear := 0
 var previous_angle := 0.0
-var gear := 1
 
-@onready var target: Marker2D = $CameraScale/CameraTarget
+@onready var camera_target: Marker2D = $CameraScale/CameraTarget
 
 
 func _process(delta: float) -> void:
-	var target_position := body.linear_velocity
-	target_position.y /= 2
-	target.position = lerp(target.position, body.linear_velocity, delta * 2)
+	camera_target.position = lerp(
+		camera_target.position,
+		body.linear_velocity,
+		delta * camera_target_sensitivity,
+	)
 
 
 func _physics_process(_delta) -> void:
-	var angle := get_angle_to(get_global_mouse_position())
-	var delta_angle := angle - previous_angle
-	previous_angle = angle
-	if delta_angle > PI:
-		delta_angle -= 2 * PI
-	elif delta_angle < -PI:
-		delta_angle += 2 * PI
-	body.apply_torque_impulse(delta_angle * force_multiplier * GEAR_RATIOS[gear])
-	line.rotation = angle
+	var angle_delta := control_point.angle - previous_angle
+	if angle_delta > PI:
+		angle_delta -= 2 * PI
+	elif angle_delta < -PI:
+		angle_delta += 2 * PI
+	previous_angle = control_point.angle
+	body.apply_torque_impulse(angle_delta * force_multiplier * GEAR_RATIOS[gear])
 	var zoom := sqrt(1.0 / (1.0 + body.linear_velocity.length() / 1000.0))
 	camera.set_zoom(lerp(camera.zoom, Vector2(zoom, zoom), 0.01))
 
@@ -43,4 +44,3 @@ func _input(event: InputEvent) -> void:
 		gear = min(gear + 1, 1)
 	elif event.is_action_pressed("gear_down"):
 		gear = max(gear - 1, 0)
-	line.default_color = GEAR_COLORS[gear]
